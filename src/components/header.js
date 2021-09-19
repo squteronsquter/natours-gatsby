7a26be4264eadf07f4c565548c6a034e09147087
squteronsquter <squter@squter.pl>
2021-09-19 15:41:18 +0200

    header with background clipping logo and heading text

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,27 @@
-import * as React from "react"
-import styled from "styled-components"
-import PropTypes from "prop-types"
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <>
-    <Wrapper>
-      <header>
-        <h1>
-          <Link to="/">{siteTitle}</Link>
-        </h1>
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="logo-box">
+          <Link to="/">
+            <StaticImage
+              src="../images/logo.svg"
+              alt="Natours logo"
+              width={50}
+            />
+          </Link>
+        </div>
+        <div className="message-box">
+          <h1 className="heading">
+            <span className="heading-top">outdoors</span>
+            <span className="heading-bottom">is where life happens</span>
+          </h1>
+        </div>
       </header>
-    </Wrapper>
-  </>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+    </>
+  )
 }
-
-Header.defaultProps = {
-  siteTitle: `natours`,
-}
-
-const Wrapper = styled.section``
-
-export default Header
